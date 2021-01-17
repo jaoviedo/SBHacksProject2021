9eec2964aff22ecc900205f2be820b4ddc2cb7ca
jaoviedo <jaoviedo@umail.ucsb.edu>
2021-01-16 18:43:23 -0800

    Baby's firsts steps

--- index.js
+++ index.js
@@ -2,3 +2,35 @@
 window.onload = function() {
     this.alert("Loaded!");
 }
+
+function changeColor1() {
+    var newColor = prompt("Please Enter your Color:", "Color")
+    if(newColor != null){
+        var elem = document.getElementById('circle1');
+        elem.style.background = newColor;
+    }
+  }
+  function changeColor2() {
+    var newColor = prompt("Please Enter your Color:", "Color")
+    if(newColor != null){
+        var elem = document.getElementById('circle2');
+        elem.style.background = newColor;
+    }
+  }
+  function changeColor3() {
+    var newColor = prompt("Please Enter your Color:", "Color")
+    if(newColor != null){
+        var elem = document.getElementById('circle3');
+        elem.style.background = newColor;
+    }
+  }
+  function changeColor4() {
+    var newColor = prompt("Please Enter your Color:", "Color")
+    if(newColor != null){
+        var elem = document.getElementById('circle4');
+        elem.style.background = newColor;
+    }
+  }
+  function evalGuess(){
+      
+  }
